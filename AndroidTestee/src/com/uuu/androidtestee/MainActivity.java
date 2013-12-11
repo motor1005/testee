5ab01db07bf0dc39d1c99bbec2feea8fb3bab977
Mark Ho <mark@gmail.com>
2013-12-11 20:47:21 +0800

    Add Utility.java

--- AndroidTestee/src/com/uuu/androidtestee/MainActivity.java
+++ AndroidTestee/src/com/uuu/androidtestee/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +15,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
